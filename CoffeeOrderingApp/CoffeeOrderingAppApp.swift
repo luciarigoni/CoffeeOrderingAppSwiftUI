40d0a3314a2718611b64f150c6408868fe53d881
luciarigoni <luciarigoni@live.com>
2022-11-14 09:12:57 -0300

    Initial Commit

---
+++ CoffeeOrderingApp/CoffeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingAppApp.swift
+//  CoffeeOrderingApp
+//
+//  Created by Lucia Rigoni Freire on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
